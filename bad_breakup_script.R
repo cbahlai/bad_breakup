b8c1a4a8c92d53d4eb2703752878b70e697859f0
Christie Bahlai <cbahlai@msu.edu>
2016-10-05 10:22:17 -0400

    Add breakup function, mess with test data a bit

--- bad_breakup_script.R
+++ bad_breakup_script.R
@@ -34,7 +34,9 @@ linefit<-function (data){
   #fit the model
   model<-lm(stand.response~year, data=data)
   #create a vector of relevant outputs. We want slope, error, P value
-  output<-c(summary(model)$coefficients[2,1], # slope
+  output<-c(min(data$year), #year the analysis started on
+            nrow(data), #number of years the analysis includes
+            summary(model)$coefficients[2,1], # slope
             summary(model)$coefficients[2,2], # se for slope
             summary(model)$coefficients[2,4]) #p value
   return(output)
@@ -44,5 +46,27 @@ linefit<-function (data){
 linefit(test1)
 # functional!
 
+#now we need to think about how to iterate through the dataset. We want a
+#function that starts at the first year, counts the number of rows specified
+#and then feeds that rsultant data frame to the fittling function. 
+#then we want to discard the first row of the data set, and repeat until fewer than
+#the number of rows specified remain
 
+breakup<-function(data, window){ #window is the size of the window we want to use
+  remaining<-data #create dummy data set to operate on
+  output<-data.frame(year=integer(0), #create empty data frame to put our output variables in
+                     length=integer(0), 
+                     slope=numeric(0), 
+                     slope_se=numeric(0), 
+                     p_value=numeric(0))
+  while (nrow(remaining)>(window-1)){ #while there's still more rows of data than in the window
+    chunk<-remaining[1:window,] #pull out a chunk as big as the window from the top of the data
+    out<-linefit(chunk) #fit a linear model and get relevant statistics on chunk
+    output<-rbind(output, out) #append the stats to the output data frame
+    remaining<-remaining[-c(1),] #cut out the first line of the remaining data + repeat
+  }
+  names(output)<-c("year", "length", "slope", "slope_se", "p_value")
+  return(output)#output the data frame
+}
 
+breakup(test1, 3)
